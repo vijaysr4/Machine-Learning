bc196978305acd013b6df5f99bcd2210522b5f40
Vijay Venkatesh <76570933+vijaysr4@users.noreply.github.com>
2021-10-26 21:35:11 +0530

    Update LogisticR.ML.py

--- Logistic Regression/LogisticR.ML.py
+++ Logistic Regression/LogisticR.ML.py
@@ -1,10 +1,12 @@
+#importing libraries 
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-from sklearn.model_selection import train_test_split
 
+#importing datasets
 dataset = pd.read_csv('C:/Users/vijay/Desktop/ML/Logistic Regression/Heart.csv')
 
+#Declaring dependent and independent variables
 X = dataset.iloc[:,:-1].values
 Y = dataset.iloc[:,-1].values
 
@@ -12,13 +14,17 @@ Y = dataset.iloc[:,-1].values
 pred = dataset.drop("target", axis = 1)
 tar = dataset["target"]
 
+#Splitting datasets into train and test
+from sklearn.model_selection import train_test_split
 X_train,X_test,Y_train,Y_test = train_test_split(X,Y,test_size = 0.25, random_state = 0)
 
+#Feature Scaling
 from sklearn.preprocessing import StandardScaler
 st_x = StandardScaler()
 X_train = st_x.fit_transform(X_train)
 X_test = st_x.transform(X_test)
 
+#Logistic Regression Algorithm
 from sklearn.linear_model import LogisticRegression
 classifier = LogisticRegression(random_state = 0)
 classifier.fit(X_train,Y_train)
