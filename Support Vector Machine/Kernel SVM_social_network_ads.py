632df3448215c71ef575378f14aecfdccfc9de23
Vijay Venkatesh <76570933+vijaysr4@users.noreply.github.com>
2021-10-27 23:27:09 +0530

    Update Kernel SVM_social_network_ads.py

--- Support Vector Machine/Kernel SVM_social_network_ads.py
+++ Support Vector Machine/Kernel SVM_social_network_ads.py
@@ -42,8 +42,11 @@ from matplotlib.colors import ListedColormap
 X_set, y_set = sc.inverse_transform(x_train), y_train
 X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 10, stop = X_set[:, 0].max() + 10, step = 1),
                      np.arange(start = X_set[:, 1].min() - 1000, stop = X_set[:, 1].max() + 1000, step = 1))
-plt.contourf(X1, X2, classifier.predict(sc.transform(np.array([X1.ravel(), X2.ravel()]).T)).reshape(X1.shape),
-             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.contourf(X1, X2,
+             classifier.predict(sc.transform(np.array([X1.ravel(), X2.ravel()]).T)).reshape(X1.shape),
+             alpha = 0.75,
+             cmap = ListedColormap(('red', 'green')))
+
 plt.xlim(X1.min(), X1.max())
 plt.ylim(X2.min(), X2.max())
 for i, j in enumerate(np.unique(y_set)):
@@ -51,6 +54,7 @@ for i, j in enumerate(np.unique(y_set)):
                 X_set[y_set == j, 1],
                 c = ListedColormap(('red', 'green'))(i),
                 label = j)
+    
 plt.title('kernel SVM (Training set)')
 plt.xlabel('Age')
 plt.ylabel('Estimated Salary')
@@ -62,8 +66,11 @@ from matplotlib.colors import ListedColormap
 X_set, y_set = sc.inverse_transform(x_test), y_test
 X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 10, stop = X_set[:, 0].max() + 10, step = 0.25),
                      np.arange(start = X_set[:, 1].min() - 1000, stop = X_set[:, 1].max() + 1000, step =0.25))
-plt.contourf(X1, X2, classifier.predict(sc.transform(np.array([X1.ravel(), X2.ravel()]).T)).reshape(X1.shape),
-             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.contourf(X1, X2, 
+             classifier.predict(sc.transform(np.array([X1.ravel(), X2.ravel()]).T)).reshape(X1.shape),
+             alpha = 0.75,
+             cmap = ListedColormap(('red', 'green')))
+
 plt.xlim(X1.min(), X1.max())
 plt.ylim(X2.min(), X2.max())
 for i, j in enumerate(np.unique(y_set)):
@@ -71,6 +78,7 @@ for i, j in enumerate(np.unique(y_set)):
                 X_set[y_set == j, 1],
                 c = ListedColormap(('red', 'green'))(i),
                 label = j)
+    
 plt.title('kernel SVM (Test set)')
 plt.xlabel('Age')
 plt.ylabel('Estimated Salary')
