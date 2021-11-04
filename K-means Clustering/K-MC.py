94bc078eed8ad365a72cda93a006e0bcea395824
Vijay Venkatesh <76570933+vijaysr4@users.noreply.github.com>
2021-11-04 22:34:48 +0530

    Update K-MC.py

--- K-means Clustering/K-MC.py
+++ K-means Clustering/K-MC.py
@@ -1,31 +1,33 @@
-#importing libraries
+# importing libraries
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-#importing datasets
+# importing datasets
 data_set = pd.read_csv('C:/Users/vijay/Desktop/ML/K-means Clustering/Mall_Customers.csv')
 X = data_set.iloc[:, [3, 4]].values
 
 #Using the elbow method to find the optimal number of clusters
 from sklearn.cluster import KMeans
 wcss = []
+
 for i in range(1, 11):
     kmeans = KMeans(n_clusters = i, init = 'k-means++', random_state = 42)
     kmeans.fit(X)
     wcss.append(kmeans.inertia_)
+    
 plt.plot(range(1, 11), wcss)
 plt.title('The Elbow Curve')
 plt.xlabel('Number of clusters')
 plt.ylabel('wcss')
 plt.show()
 
-#Training the K-Means model on the dataset
+# Training the K-Means model on the dataset
 kmeans = KMeans(n_clusters = 5, init = 'k-means++', random_state = 42)
 y_kmeans = kmeans.fit_predict(X)
 print(y_kmeans)
 
-#Visualising the clusters
+# Visualising the clusters
 plt.scatter(X[y_kmeans == 0,0], X[y_kmeans == 0, 1], s = 100, c = 'red', label = 'cluster 1')
 plt.scatter(X[y_kmeans == 1,0], X[y_kmeans == 1, 1], s = 100, c = 'blue', label = 'cluster 2')
 plt.scatter(X[y_kmeans == 2,0], X[y_kmeans == 2, 1], s = 100, c = 'green', label = 'cluster 3')
