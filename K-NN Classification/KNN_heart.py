578ac11fa1b95621467a3a7859614b9845da8512
Vijay Venkatesh <76570933+vijaysr4@users.noreply.github.com>
2021-11-05 23:21:02 +0530

    Update KNN_heart.py

--- K-NN Classification/KNN_heart.py
+++ K-NN Classification/KNN_heart.py
@@ -1,38 +1,35 @@
-#importing libraries
+# importing libraries
 import numpy as np 
 import pandas as pd
 import matplotlib.pyplot as plt
 
-
-#importing dataset
+# importing dataset
 data_set = pd.read_csv('C:/Users/vijay/Desktop/ML/K-NN Classification/Heart.csv')
 
 x = data_set.iloc[:,:-1].values
 y = data_set.iloc[:,-1].values
 
-
 pred = data_set.drop("target", axis = 1)
 tar = data_set["target"]
 
-#splitting datasets
+# splitting datasets
 from sklearn.model_selection import train_test_split
 x_train,x_test,y_train,y_test = train_test_split(x,y,test_size = 0.25, random_state = 0)
 
-#Feature Scalling
+# Feature Scalling
 from sklearn.preprocessing import StandardScaler
 st_x = StandardScaler()
 x_train = st_x.fit_transform(x_train)
 x_test = st_x.fit_transform(x_test)
 
-#Training K_NN Classifier to the datasets
+# Training K_NN Classifier to the datasets
 from sklearn.neighbors import KNeighborsClassifier
 classifier = KNeighborsClassifier(n_neighbors = 5)
 classifier.fit(x_train,y_train)
 
 
-#Predicting the test & train results
+# Predicting the test & train results
 y_pred = classifier.predict(x_test)
-
 
 
 print('Train Score:', classifier.score(x_train,y_train))
