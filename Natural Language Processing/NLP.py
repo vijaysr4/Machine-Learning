49175250ef4956cd886b5f4ece8f8a046a73ecbf
Vijay Venkatesh <76570933+vijaysr4@users.noreply.github.com>
2021-11-06 23:43:19 +0530

    Update NLP.py

--- Natural Language Processing/NLP.py
+++ Natural Language Processing/NLP.py
@@ -13,6 +13,7 @@ nltk.download('stopwords')
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 corpus = []
+
 for i in range(0, 1000):
   review = re.sub('[^a-zA-Z]', ' ', dataset['Review'][i])
   review = review.lower()
