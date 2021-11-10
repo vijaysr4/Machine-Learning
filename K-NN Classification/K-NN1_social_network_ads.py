dec603fe9cebc69ce6e77b8b51652611544a63ce
Vijay Venkatesh <76570933+vijaysr4@users.noreply.github.com>
2021-11-10 23:25:31 +0530

    Update K-NN1_social_network_ads.py

--- K-NN Classification/K-NN1_social_network_ads.py
+++ K-NN Classification/K-NN1_social_network_ads.py
@@ -9,7 +9,7 @@ data_set = pd.read_csv('C:/Users/vijay/Desktop/ML/K-NN Classification/Social_Net
 x = data_set.iloc[:,:-1].values
 y = data_set.iloc[:,-1].values
 
-#splitting datasets
+# Splitting datasets
 from sklearn.model_selection import train_test_split
 x_train,x_test,y_train,y_test = train_test_split(x,y,test_size = 0.2, random_state=0)
 
