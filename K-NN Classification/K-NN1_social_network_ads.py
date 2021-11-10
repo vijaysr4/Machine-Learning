1316124ca43c71be1efc7e2e0ddd17d056e9c2a0
Vijay Venkatesh <76570933+vijaysr4@users.noreply.github.com>
2021-11-10 23:25:22 +0530

    Update K-NN1_social_network_ads.py

--- K-NN Classification/K-NN1_social_network_ads.py
+++ K-NN Classification/K-NN1_social_network_ads.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt 
 import pandas as pd
 
-#importing dataset
+# Importing dataset
 data_set = pd.read_csv('C:/Users/vijay/Desktop/ML/K-NN Classification/Social_Network_Ads.csv')
 
 x = data_set.iloc[:,:-1].values
