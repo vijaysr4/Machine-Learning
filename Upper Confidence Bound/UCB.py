52e569f66b89f1a66cff835581917bdcc1ac57ba
Vijay Venkatesh <76570933+vijaysr4@users.noreply.github.com>
2021-12-03 23:07:13 +0530

    Update UCB.py

--- Upper Confidence Bound/UCB.py
+++ Upper Confidence Bound/UCB.py
@@ -3,7 +3,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-
 # Importing the dataset
 dataset = pd.read_csv('C:/Users/vijay/Desktop/ML/Upper Confidence Bound/Ads_CTR_Optimisation.csv')
 
