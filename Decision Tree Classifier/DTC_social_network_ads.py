dc78163ddfe250d46a7673bb52845d1a2a494603
Vijay Venkatesh <76570933+vijaysr4@users.noreply.github.com>
2021-12-09 23:57:37 +0530

    Update DTC_social_network_ads.py

--- Decision Tree Classifier/DTC_social_network_ads.py
+++ Decision Tree Classifier/DTC_social_network_ads.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt 
 import pandas as pd
 
-# Importing dataset
+# Importing datasets
 data_set = pd.read_csv('C:/Users/vijay/Desktop/ML/Decision Tree Classifier/Social_Network_Ads.csv')
 
 # Splitting dependent and independent variables
