885e08dbe7789ad1536381d36b070f27b11d9ed4
Vijay Venkatesh <76570933+vijaysr4@users.noreply.github.com>
2021-12-28 20:13:46 +0530

    Update SimpleLinearRegression.ML.py

--- Simple Linear Regression/SimpleLinearRegression.ML.py
+++ Simple Linear Regression/SimpleLinearRegression.ML.py
@@ -25,7 +25,7 @@ x_train,x_test,y_train,y_test = train_test_split(pred,tar,test_size = 0.2,random
 x_train.shape
 y_train.shape
 
-#Linear Regression Algorithm
+# Linear Regression Algorithm
 from sklearn.linear_model import LinearRegression
 regressor = LinearRegression()
 regressor.fit(x_train,y_train)
